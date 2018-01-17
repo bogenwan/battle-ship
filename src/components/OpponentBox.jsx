b8a21d3a437cb17d208015299468fc6e2d61a912
Johnny <po_bo@hotmail.com>
2018-01-16 20:30:46 -0800

    refactored code to be able to render HIT and MISS box on opponent board

--- src/components/OpponentBox.jsx
+++ src/components/OpponentBox.jsx
@@ -9,13 +9,31 @@ const checkIfBoxHit = (storageObj, currCoord) => {
 
 const OpponentBox = (props) => {
   console.log(checkIfBoxHit(props.hitAndMissStorage, props.i));
-  return (
-    <div className="opponent-box-container" onClick={() => {props.fireShots(props.i)}}>
-      <div className="opponent-box">
-
+  if (checkIfBoxHit(props.hitAndMissStorage, props.i) === 'hit') {
+    return (
+      <div className="opponent-box-container-hit" onClick={() => {props.fireShots(props.i)}}>
+        <div className="opponent-box">
+          HIT!
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else if (checkIfBoxHit(props.hitAndMissStorage, props.i) === 'miss') {
+    return (
+      <div className="opponent-box-container-miss" onClick={() => {props.fireShots(props.i)}}>
+        <div className="opponent-box">
+          MISS!
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="opponent-box-container" onClick={() => {props.fireShots(props.i)}}>
+        <div className="opponent-box">
+
+        </div>
+      </div>
+    )
+  }
 };
 
 export default OpponentBox;
